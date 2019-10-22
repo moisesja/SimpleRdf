84ab7eb2ceea2010da819d6dd5a58728765c27d1
moisesja <moisesj@outlook.com>
2019-10-21 22:48:03 -0400

    * Program.cs: * SimpleRdfConsole.csproj:
    * SimpleRdfConsole.sln: First commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using VDS.Common;
+using VDS.RDF;
+
+namespace SimpleRdfConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IGraph g = new Graph();
+
+            Console.WriteLine("Create instance of Graph");
+        }
+    }
+}
