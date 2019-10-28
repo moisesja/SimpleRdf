537b9486ccbd16ce6e51425058833418e1e8fac1
Moises E. Jaramillo <moisesj@outlook.com>
2019-10-28 18:00:22 +0000

    Prior to connecting to a solid pod

---
+++ Examples/WorkingWithGraphsExample.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using VDS.RDF;
+using VDS.RDF.Query;
+
+namespace SimpleRdfConsole.Examples
+{
+    public class WorkingWithGraphsExample : IExample
+    {
+        public void Execute()
+        {
+            //First define a SPARQL Endpoint for DBPedia
+            SparqlRemoteEndpoint endpoint = new SparqlRemoteEndpoint(new Uri("http://dbpedia.org/sparql"));
+
+            //Next define our query
+            //We're going to ask DBPedia to describe the first thing it finds which is a Person
+            String query = "DESCRIBE ?person WHERE {?person a <http://dbpedia.org/ontology/Person>} LIMIT 1";
+
+            //Get the result
+            var graph = endpoint.QueryWithResultGraph(query);
+
+            if (!graph.IsEmpty)
+            {
+                foreach (Triple t in graph.Triples)
+                {
+                    Console.WriteLine(t.ToString());
+                    Console.WriteLine("\n\n");
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,10 +6,14 @@ namespace SimpleRdfConsole
     {
         static void Main(string[] args)
         {
+            /*
             IExample example = new SimpleWriteExample();
             example.Execute();
 
             example = new SimpleReadExample();
+            example.Execute();*/
+
+            IExample example = new WorkingWithGraphsExample();
             example.Execute();
         }
     }
